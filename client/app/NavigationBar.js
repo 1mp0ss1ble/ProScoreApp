6efdcb861ca2ceff895687df942e2b8236e3cc32
Askar <askarisback@gmail.com>
2017-04-25 15:34:55 +0500

    Update NavigationBar.js

--- client/app/NavigationBar.js
+++ client/app/NavigationBar.js
@@ -4,48 +4,50 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Nav, Navbar, NavItem,NavDropdown, MenuItem} from 'react-bootstrap/lib';
 
 
-export default () => {
-	return (
-		
-	/*	<nav className="navbar navbar-default">
-			<div className="container-fluid">
-				<div className="navbar-header">
-					<Link to="/" className="navbar-brand">Match</Link>
-				</div>
-				
-				<div className="collapse navbar-collapse">
-					<ul className="nav navbar-nav navbar-right">
-						<li>
-							<Link to="/Admin">Admin</Link>							
-						</li>
-						<li>
-							<Link to="/Signup">Signup</Link>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	 */	
+class NavigationBar extends React.Component{
+	render(){
+		return (
+			/*	<nav className="navbar navbar-default">
+					<div className="container-fluid">
+						<div className="navbar-header">
+							<Link to="/" className="navbar-brand">Match</Link>
+						</div>
 
-	<Navbar collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#">ProScore</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav pullRight>
-        <LinkContainer to={{pathname: '/admin'}}>
-    		<NavItem eventKey={1} href="#/Admin">Admin</NavItem>
-    	</LinkContainer>
+						<div className="collapse navbar-collapse">
+							<ul className="nav navbar-nav navbar-right">
+								<li>
+									<Link to="/Admin">Admin</Link>							
+								</li>
+								<li>
+									<Link to="/Signup">Signup</Link>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			 */	
 
-        <LinkContainer to={{pathname: '/signup'}}>
-    		<NavItem eventKey={1} href="#/Signup">Signup</NavItem>
-    	</LinkContainer>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
- 
-	);
+			<Navbar collapseOnSelect>
+			<Navbar.Header>
+			  <Navbar.Brand>
+				<a href="#">ProScore</a>
+			  </Navbar.Brand>
+			  <Navbar.Toggle />
+			</Navbar.Header>
+			<Navbar.Collapse>
+			  <Nav pullRight>
+				<LinkContainer to={{pathname: '/admin'}}>
+					<NavItem eventKey={1} href="#/Admin">Admin</NavItem>
+				</LinkContainer>
+
+				<LinkContainer to={{pathname: '/signup'}}>
+					<NavItem eventKey={1} href="#/Signup">Signup</NavItem>
+				</LinkContainer>
+			  </Nav>
+			</Navbar.Collapse>
+		  </Navbar>
+		);
+   }
 }
+
+export default NavigationBar;
