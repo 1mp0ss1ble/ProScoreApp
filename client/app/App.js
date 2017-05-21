84460ea5e7dfff28e036c9aa5f31e7c5e3350ea1
askarisback@gmail.com <askarisback@gmail.com>
2017-05-21 17:10:30 +0500

    testing tab element

--- client/app/App.js
+++ client/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import NavigationBar from './NavigationBar';
+import { Tab, Row, Col, Nav, NavItem } from 'react-bootstrap'
 import Modal from './modal';
 import api from '../api/db';
 
@@ -25,16 +26,45 @@ class App extends React.Component {
 			<div className="content">
 
 				<NavigationBar />
-
+				{tabsInstance}
 				<div className="container">
 					 { this.props.children }
 				</div>
-
 				<Modal />
-
 			</div>
 		);
 	}
 };
 
-export default connect(state=>({loaders:state.loaders}))(App);
+export default connect(state =>
+	({loaders:state.loaders}))(App);
+
+
+
+
+	const tabsInstance = (
+	  <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+	    <Row className="clearfix">
+	      <Col sm={4}>
+	        <Nav bsStyle="pills" stacked>
+	          <NavItem eventKey="first">
+	            Tab 1
+	          </NavItem>
+	          <NavItem eventKey="second">
+	            Tab 2
+	          </NavItem>
+	        </Nav>
+	      </Col>
+	      <Col sm={8}>
+	        <Tab.Content animation>
+	          <Tab.Pane eventKey="first">
+	            Tab 1 content
+	          </Tab.Pane>
+	          <Tab.Pane eventKey="second">
+	            Tab 2 content
+	          </Tab.Pane>
+	        </Tab.Content>
+	      </Col>
+	    </Row>
+	  </Tab.Container>
+	);
